2e91fd140c028aecd654e85308c809074222a54e
Gabriel Klein <gabrielkleinribas@gmail.com>
2023-08-20 20:13:17 +0000

    Added test return on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Hello, World</h1>
+    const techs = [{ id: 1, name: 'JavaScript' }, { id: 2, name: 'NodeJS' }, { id: 3, name: 'PostgreSQL' }]
+
+    const technologies = techs.map(tech => tech.name.concat(' '))
+
+    return (
+        <>
+            <h1>{technologies}</h1>
+        </>
+    )
 }
 
 export default Home

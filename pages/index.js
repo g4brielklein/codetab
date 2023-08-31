95f2e0076cc66837daa21867ee6b00840e687eed
Gabriel Klein <gabrielkleinribas@gmail.com>
2023-08-31 01:38:23 +0000

    Added favicon link

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ function Home() {
   return (
     <>
       <div className="container">
+        <Head>
+          <link rel="shorcut icon" href="/favicon.ico" />
+        </Head>
         <h1>Under construction... 🚧</h1>
         <p>github.com/g4brielklein</p>
       </div>

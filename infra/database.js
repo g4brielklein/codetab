a56fb1e6f2f92858607d541b7fba5dc4b5d6345c
Gabriel Klein <gabrielkleinribas@gmail.com>
2023-10-30 23:51:21 +0000

    Added `database` connection property

--- infra/database.js
+++ infra/database.js
@@ -5,6 +5,7 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();

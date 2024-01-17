fb234e1dbf995471f195de02204a61d5816f5205
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-01-17 12:04:21 +0000

    Added database status info and tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,27 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
+  const currentConnections = await database.query(
+    "SELECT sum(numbackends) AS total FROM pg_stat_database;",
+  );
+
+  const maxConnections = await database.query(
+    "SELECT * FROM pg_settings WHERE name = 'max_connections';",
+  );
+
+  const postgresVersion = await database.query(
+    "SELECT split_part(version(), ' ', 2) AS version",
+  );
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnections.rows[0].setting),
+        opened_connections: parseInt(currentConnections.rows[0].total),
+        version: postgresVersion.rows[0].version,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,3 +7,20 @@ test("GET to /api/vi/status should return 200", async () => {
   expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
+
+test("Status info should be returned and valid from status page", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  const { max_connections, opened_connections, version } =
+    responseBody.dependencies.database;
+
+  expect(max_connections).toBeDefined();
+  expect(max_connections).toBeGreaterThan(0);
+
+  expect(opened_connections).toBeDefined();
+  expect(opened_connections).toBeGreaterThan(-1);
+
+  expect(version).toBeDefined();
+  expect(parseInt(version)).toBeGreaterThan(0);
+});

c7a58e8a27f2ad0c364ee09ce98f485770b37c95
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-01-20 22:52:34 +0000

    Changed `databaseName` value to process.env instead of url param

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,11 +3,9 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseName = request.query.databaseName;
-
   const currentConnections = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
+    values: [process.env.POSTGRES_DB],
   });
 
   const maxConnections = await database.query("SHOW max_connections;");

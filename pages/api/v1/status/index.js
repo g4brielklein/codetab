40ed5951bdc786eb997cbe45164d0b570afda3e4
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-01-20 22:49:44 +0000

    Updated `currentConnections` query to prevent `sql injection`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,9 +5,10 @@ async function status(request, response) {
 
   const databaseName = request.query.databaseName;
 
-  const currentConnections = await database.query(
-    `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`,
-  );
+  const currentConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
   const maxConnections = await database.query("SHOW max_connections;");
 

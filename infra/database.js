8630d19234a1de80ad8a05121b43fe7dc8dcfd52
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-01-21 04:19:20 +0000

    Fixed connections not closing on query error

--- infra/database.js
+++ infra/database.js
@@ -8,11 +8,16 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
 
-  return result;
+  await client.connect();
+
+  try {
+    return await client.query(queryObject);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {

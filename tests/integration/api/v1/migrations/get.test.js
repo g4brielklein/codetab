f5a4eafec103b48eb90bdc0eb7a6432d827ffa86
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-03-12 11:35:50 +0000

    Added test for migrations `dry run` endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,7 @@
+test("Should run migrations on dry run", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody.pendingMigrations)).toBe(true);
+});

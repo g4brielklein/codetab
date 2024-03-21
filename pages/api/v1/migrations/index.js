e747873399fc736be9b56b957c30ac9db163032a
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-03-21 11:56:33 +0000

    Configurds `POST` /migrations endpointand test runner script

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,14 +2,28 @@ import nodePgMigrate from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const pendingMigrations = await nodePgMigrate({
-    databaseUrl: process.env.DATABASE_URL,
-    dir: join("infra", "migrations"),
-    migrationsTable: "pgmigrations",
-    direction: "up",
-    dryRun: true,
-    verbose: true,
-  });
+  let pendingMigrations = [];
+  const method = request.method;
+
+  if (method === "GET") {
+    pendingMigrations = await nodePgMigrate({
+      databaseUrl: process.env.DATABASE_URL,
+      dir: join("infra", "migrations"),
+      migrationsTable: "pgmigrations",
+      direction: "up",
+      dryRun: true,
+      verbose: true,
+    });
+  } else if (method === "POST") {
+    pendingMigrations = await nodePgMigrate({
+      databaseUrl: process.env.DATABASE_URL,
+      dir: join("infra", "migrations"),
+      migrationsTable: "pgmigrations",
+      direction: "up",
+      dryRun: false,
+      verbose: true,
+    });
+  }
 
   response.send({
     pendingMigrations,

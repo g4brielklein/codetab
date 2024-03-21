6f7a5068114df591508102b2fe1ffbaea958a139
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-03-21 12:00:23 +0000

    Adjusted `/migrations` GET test and created POST test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,10 @@
+import database from "infra/database";
+
 test("Should run migrations on dry run", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody.pendingMigrations)).toBe(true);
+  expect(responseBody.pendingMigrations.length).toBeGreaterThan(0);
 });
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,7 @@
+test("Should run migrations on live run", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+
+  expect(response.status).toBe(200);
+});

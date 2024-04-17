0087515f20ea5766e07f78ccc1faa15c41920ac0
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-04-17 23:58:58 +0000

    Added function to clean database before tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ const nextJest = require("next/jest.js");
 const dotenv = require("dotenv");
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const createJestConfig = nextJest();
+const createJestConfig = nextJest({
+  dir: "./",
+});
 
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,8 +1,16 @@
 import database from "infra/database";
 
+async function cleanDB() {
+  await database.query(`
+    DROP SCHEMA public; CREATE SCHEMA public;
+  `);
+}
+
 test("Should run migrations on dry run", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
+
+  await cleanDB();
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody.pendingMigrations)).toBe(true);

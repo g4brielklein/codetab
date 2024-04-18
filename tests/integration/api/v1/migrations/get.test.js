459727e439ee0a67649ee0dcdad041f1c19070a3
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-04-18 23:44:43 +0000

    Added `beforeAll` function and cascade on clean database

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,16 +1,18 @@
 import database from "infra/database";
 
+beforeAll(cleanDB);
+
 async function cleanDB() {
   await database.query(`
-    DROP SCHEMA public; CREATE SCHEMA public;
+    DROP SCHEMA public cascade; CREATE SCHEMA public;
   `);
+
+  console.log("Database cleaned for tests");
 }
 
 test("Should run migrations on dry run", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
-
-  await cleanDB();
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody.pendingMigrations)).toBe(true);

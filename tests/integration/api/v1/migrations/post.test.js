2e9bc6d821ea3ccdb5e17338add0b56069d035fc
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-04-18 23:45:41 +0000

    Improved migrations `post` test

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,22 @@
 import database from "infra/database";
 
+beforeAll(cleanDB);
+
+async function cleanDB() {
+  await database.query(`
+    DROP SCHEMA public cascade; CREATE SCHEMA public;
+  `);
+
+  console.log("Database cleaned for tests");
+}
+
 test("Should run migrations on live run", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-
   expect(response.status).toBe(200);
+
+  const getResponse = await fetch("http://localhost:3000/api/v1/migrations");
+  const getResponseJson = await getResponse.json();
+  expect(getResponseJson.pendingMigrations.length).toBe(0);
 });

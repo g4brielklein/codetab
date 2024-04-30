ab477ad2f2a574569abbc660931482f311f32fa7
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-04-30 00:01:01 +0000

    added specific object responses and status for POST and GET requests

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,17 +3,41 @@ import { join } from "node:path";
 
 export default async function migrations(request, response) {
   const method = request.method;
+  let status = 200;
 
-  const pendingMigrations = await nodePgMigrate({
+  if (method !== "POST" && method !== "GET") {
+    return response
+      .status(403)
+      .send({ ERROR: `Method ${method} not allowed on this endpoint` });
+  }
+
+  const defaultMigrationOptions = {
     databaseUrl: process.env.DATABASE_URL,
     dir: join("infra", "migrations"),
     migrationsTable: "pgmigrations",
     direction: "up",
-    dryRun: method === "POST" ? false : true,
+    dryRun: true,
     verbose: true,
-  });
+  };
 
-  response.send({
+  if (method === "POST") {
+    const runnedMigrations = await nodePgMigrate({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    status = runnedMigrations.length >= 1 ? (status = 201) : status;
+
+    return response.status(status).send({
+      runnedMigrations: runnedMigrations.map(
+        (runnedMigration) => runnedMigration.name,
+      ),
+    });
+  }
+
+  const pendingMigrations = await nodePgMigrate(defaultMigrationOptions);
+
+  return response.status(status).send({
     pendingMigrations: pendingMigrations.map(
       (pendingMigration) => pendingMigration.name,
     ),

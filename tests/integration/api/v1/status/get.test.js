82f02bc26f66e929eb388ac8d7acbfc9d50e2bec
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-09-21 18:45:05 +0000

    fix test name

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,7 +4,7 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices()
 })
 
-test("GET to /api/vi/status should return 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
   expect(response.status).toBe(200);

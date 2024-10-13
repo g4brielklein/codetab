526876a4238bc4718d2c12b4090cef55cb435eee
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-10-13 19:57:17 +0000

    test: add `describe` function to to GET migrations and status tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -12,12 +12,16 @@ async function cleanDB() {
   `);
 }
 
-test("Should run migrations on dry run", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations on dry run", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody.pendingMigrations)).toBe(true);
-  expect(response.status).toBe(200);
-  expect(responseBody.pendingMigrations.length).toBeGreaterThan(0);
+      const responseBody = await response.json();
+      expect(Array.isArray(responseBody.pendingMigrations)).toBe(true);
+      expect(response.status).toBe(200);
+      expect(responseBody.pendingMigrations.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,29 +4,33 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Accessing status page", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+      expect(response.status).toBe(200);
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-});
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+    });
 
-test("Status info should be returned and valid from status page", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
+    test("Getting database info", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
 
-  const { max_connections, opened_connections, version } =
-    responseBody.dependencies.database;
+      const { max_connections, opened_connections, version } =
+        responseBody.dependencies.database;
 
-  expect(max_connections).toBeDefined();
-  expect(max_connections).toBeGreaterThan(0);
+      expect(max_connections).toBeDefined();
+      expect(max_connections).toBeGreaterThan(0);
 
-  expect(opened_connections).toBeDefined();
-  expect(opened_connections).toBe(1);
+      expect(opened_connections).toBeDefined();
+      expect(opened_connections).toBe(1);
 
-  expect(version).toBeDefined();
-  expect(parseInt(version)).toBeGreaterThan(0);
+      expect(version).toBeDefined();
+      expect(parseInt(version)).toBeGreaterThan(0);
+    });
+  });
 });

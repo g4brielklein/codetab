0c426d284de92b137792958330453ea2913da209
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-10-19 02:40:52 +0000

    chore: add `husky` pre-commit hook

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,10 +2,6 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const variable
-  const credential = 'ghp_hntxGk25ziho1fJa1wDYUwMxXLxqAO3SGVQS'
-  const apiKey = 'sk_test_4eC39HqLYJNDbrjtT1zdp7dc'
-
 
   const currentConnections = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",

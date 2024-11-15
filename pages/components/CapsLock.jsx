b3fced4ea7ee4998b7aa8c4d97bd2f4bb3461a0e
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-11-15 17:55:36 +0000

    feat: change export to test Vercel deploy

--- pages/components/CapsLock.jsx
+++ pages/components/CapsLock.jsx
@@ -1,5 +1,9 @@
-export default function CapsLock(props) {
+function CapsLock(props) {
   const { text } = props;
+
+  console.log(text);
 
   return <h1>{text.toUpperCase()}</h1>;
 }
+
+export default CapsLock;

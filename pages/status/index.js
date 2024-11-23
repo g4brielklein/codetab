57d81419b788a05e7ddc1e15da8ae91b604427f6
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-11-23 20:25:57 +0000

    feat: add `swr` fetcher to get status

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,7 +1,24 @@
-import CapsLock from "../components/CapsLock";
+import useSWR from "swr";
 
 function Status() {
-  return <CapsLock text="Status page from Codetab" />;
+  const { data, error, isLoading } = useSWR("/api/v1/status", fetcher, {
+    refreshInterval: 60000,
+  });
+
+  async function fetcher(key) {
+    const response = await fetch(key);
+    return response.json();
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
+  }
+
+  if (isLoading) {
+    return <h2>Loading data...</h2>;
+  }
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
 export default Status;

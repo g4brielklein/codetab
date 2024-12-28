e63cbb93de082ac2ef25bb6aef7b2d1d93a8639c
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-12-28 21:43:05 +0000

    feat: add try/catch clause on status page api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,27 +1,40 @@
 import database from "infra/database.js";
 
+import { InternalServerError } from 'infra/errors.js';
+
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const currentConnections = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [process.env.POSTGRES_DB],
-  });
+    const currentConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [process.env.POSTGRES_DB],
+    });
 
-  const maxConnections = await database.query("SHOW max_connections;");
+    const maxConnections = await database.query("SHOW max_connections;");
 
-  const postgresVersion = await database.query("SHOW server_version;");
+    const postgresVersion = await database.query("SHOW server_version;");
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        max_connections: parseInt(maxConnections.rows[0].max_connections),
-        opened_connections: currentConnections.rows[0].count,
-        version: postgresVersion.rows[0].server_version,
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          max_connections: parseInt(maxConnections.rows[0].max_connections),
+          opened_connections: currentConnections.rows[0].count,
+          version: postgresVersion.rows[0].server_version,
+        },
       },
-    },
-  });
+    });
+  } catch(err) {
+    const error = new InternalServerError({
+      message: "Internal Server Error",
+      cause: err
+    });
+
+    console.error(error);
+
+    response.status(error.statusCode).json(error);
+  }
 }
 
 export default status;

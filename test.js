c5f2c539c715b7cdb0a68b9741df839d151cf2d4
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-08-25 23:56:12 +0000

    feat: add custom errors

---
+++ test.js
@@ -0,0 +1,36 @@
+class ValidationError extends Error {
+  constructor(message, options = {}) {
+    const { statusCode = 400 } = options;
+
+    super(message);
+    this.status_code = statusCode;
+  }
+}
+
+function saveUser(input) {
+  if (!input) {
+    throw new ReferenceError("Input argument is required");
+  }
+
+  if (!input.name) {
+    throw new ValidationError("Name is required");
+  }
+
+  user.save(input);
+}
+
+try {
+  saveUser({
+    lastname: "Doe",
+  });
+} catch (err) {
+  if (err instanceof ReferenceError) {
+    return console.log(err);
+  }
+
+  if (err instanceof ValidationError) {
+    return console.log(err);
+  }
+
+  console.log(err);
+}

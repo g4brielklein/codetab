a360e43959d6725cb2689ed89a73664e6037a282
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-08-29 00:34:11 +0000

    test: add test to ensure no `POST` allowed on `/status` endpoint

---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,17 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("POST /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Making a post request", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status", {
+        method: "POST",
+      });
+
+      expect(response.status).toBe(405);
+    });
+  });
+});
